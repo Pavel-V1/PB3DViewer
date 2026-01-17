87040f96828f50638ce23f1554488994ffeff51b
Pavel-V1 <pavelvoronov009@gmail.com>
2026-01-17 03:18:49 +0300

    добавил триангуляцию и почти сделал тесты

--- src/main/java/app/rend_prepare/ModelTriangulation.java
+++ src/main/java/app/rend_prepare/ModelTriangulation.java
@@ -1,4 +1,43 @@
 package app.rend_prepare;
 
+import app.model.Model;
+import app.model.Polygon;
+import app.model.Vertex;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModelTriangulation {
+
+    public static Model triangulate(Model model) {
+        List<Polygon> polygons = triangulate(model.getPolygons(), model.getVertices());
+        Model newModel = new Model();
+        newModel.getTexCoords().addAll(model.getTexCoords());
+        newModel.getNormals().addAll(model.getNormals());
+        newModel.getVertices().addAll(model.getVertices());
+        newModel.getPolygons().addAll(polygons);
+        return newModel;
+    }
+
+    private static List<Polygon> triangulate(List<Polygon> polygons, List<Vertex> modelVertices) {
+        List<Polygon> triangles = new ArrayList<>();
+
+        for (Polygon polygon : polygons) {
+            List<Vertex> vertices = new ArrayList<>();
+            for (Integer index : polygon.vertexIndices()) {
+                vertices.add(modelVertices.get(index));
+            }
+            int n = vertices.size();
+
+            for (int i = 1; i < n - 1; i++) {
+                List<Integer> v = new ArrayList<>();
+                v.add(0);
+                v.add(i);
+                v.add(i + 1);
+                Polygon triangle = new Polygon(v, polygon.texCoordIndices(), polygon.normalIndices());
+                triangles.add(triangle);
+            }
+        }
+
+        return triangles;
+    }
 }
--- src/test/java/app/rend_prepare/PrepareTest.java
+++ src/test/java/app/rend_prepare/PrepareTest.java
@@ -1,4 +1,54 @@
 package app.rend_prepare;
 
+import app.model.Model;
+import app.model.Polygon;
+import app.model.Vertex;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrepareTest {
+    @Test
+    public void triangulationTest() {
+        Model model = new Model();
+        List<Vertex> vertices = new ArrayList<>();
+        vertices.add(new Vertex(0, 1, 0));
+        vertices.add(new Vertex(0, 2, 0));
+        vertices.add(new Vertex(1, 0, 0));
+        vertices.add(new Vertex(1, 3, 0));
+        vertices.add(new Vertex(2, 1, 0));
+        vertices.add(new Vertex(2, 2, 0));
+        vertices.add(new Vertex(0, 1, 1));
+        vertices.add(new Vertex(0, 2, 1));
+        vertices.add(new Vertex(1, 1, 1));
+        model.getVertices().addAll(vertices);
+
+        List<Integer> vi = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            vi.add(i);
+        }
+        model.getPolygons().add(new Polygon(vi, null, null));
+        vi.clear();
+        for (int i = 4; i < 8; i++) {
+            vi.add(i);
+        }
+        model.getPolygons().add(new Polygon(vi, null, null));
+        vi.clear();
+        for (int i = 6; i < 9; i++) {
+            vi.add(i);
+        }
+        model.getPolygons().add(new Polygon(vi, null, null));
+
+        Model newModel = ModelTriangulation.triangulate(model);
+        List<Polygon> newPolygons = new ArrayList<>();
+        //
+        Assertions.assertEquals(newPolygons, newModel.getPolygons());
+    }
+
+    @Test
+    public void normalsCalculateTest(){
+        //
+    }
 }
